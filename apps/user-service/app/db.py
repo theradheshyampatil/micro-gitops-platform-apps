4f88373b2838583ba8fd404323b25266462658d0
theradheshyampatil <radheshyam9096@gmail.com>
2026-01-12 08:15:45 +0000

    fix(user-service): fix db.py syntax error causing CrashLoop

---
+++ apps/user-service/app/db.py
@@ -0,0 +1,13 @@
+import os
+import psycopg2
+
+def get_connection():
+    return psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        sslmode="require"
+    )
+
